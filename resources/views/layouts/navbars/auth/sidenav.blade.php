1226668c3d34966644b11ebca4012d464aef35d8
SuryaImg <suryabhan.img@gmail.com>
2024-09-10 15:45:43 +0530

    design changes

--- resources/views/layouts/navbars/auth/sidenav.blade.php
+++ resources/views/layouts/navbars/auth/sidenav.blade.php
@@ -5,7 +5,8 @@
             aria-hidden="true" id="iconSidenav"></i>
         <a class="navbar-brand m-0" href="{{ route('dashboard') }}"
             target="_blank">
-            <img src="{{ asset('assets/img/logo-ct-dark.png') }}" class="navbar-brand-img h-100" alt="main_logo">
+            <!-- <img src="{{ asset('assets/img/logo-ct-dark.png') }}" class="navbar-brand-img h-100" alt="main_logo"> -->
+            <img src="{{ asset('assets/img/advocate-logo.png') }}" class="navbar-brand-img h-100" alt="main_logo">
             <span class="ms-1 font-weight-bold">Admin Panel</span>
         </a>
     </div>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -37,7 +37,7 @@
                                     <td>
                                         <div class="d-flex px-3 py-1">
                                             <div>
-                                                <img src="{{ asset('assets/img/team-1.jpg') }}" class="avatar me-3" alt="image">
+                                                <img src="{{ asset('assets/img/adv.png') }}" class="avatar me-3" alt="image">
                                             </div>
                                             <div class="d-flex flex-column justify-content-center">
                                                 <h6 class="mb-0 text-sm">{{$user->name}}</h6>
